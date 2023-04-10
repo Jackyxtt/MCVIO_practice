8ab210ad02370a107ac6903cc2729c96fb2c9bcb
Jackyxtt <944766685@qq.com>
2023-04-10 16:45:08 +0800

    finish testing for adding sensors

--- MCVIO/src/MCVIOestimator_node.cpp
+++ MCVIO/src/MCVIOestimator_node.cpp
@@ -1,4 +1,5 @@
 #include <ros/ros.h>
+#include <glog/logging.h>
 #include "Estimator/parameters.h"
 #include "Frontend/MCVIOfrontend.h"
 
@@ -9,12 +10,21 @@ int main(int argc, char **argv){
     ros::init(argc, argv, "MCVIO_estimator");
     ros::NodeHandle n("~");
     ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME, ros::console::levels::Info);
+
+    string log_dir;
+    n.getParam("log_dir", log_dir);
+    FLAGS_log_dir = log_dir;
+    FLAGS_alsologtostderr = 1;
+    google::InitGoogleLogging("MCVIO_practice");
+
     readParameters(n);
 
     string config_file = readParam<string>(n, "config_file");
 
     MCVIOfrontend frontend(config_file);
     MCVIOfrontend_ = &frontend;
-    // frontend.setUpROS(nullptr, &n);
+    
+    frontend.setUpROS(nullptr, &n);
+    google::ShutdownGoogleLogging();
 
 }
